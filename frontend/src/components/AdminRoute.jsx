e40d3e2895eb00d0a952d3bb7710601e010a6de7
Mazda Nawallsyah <mazdanawallsyah2601@gmail.com>
2025-09-09 11:55:03 +0700

    lucu nih admin routing

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,32 +1,25 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { UserContext } from "./LandingPage/UserContext.jsx"; 
-import { Spinner } from "react-bootstrap";
+import { UserContext } from "./LandingPage/UserContext.jsx";
+import Transition from "./LandingPage/Transition.jsx";
 
 const AdminRoute = ({ children }) => {
   const { user, loading } = useContext(UserContext);
   const location = useLocation();
 
   if (loading) {
-    return (
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <Spinner animation="border" />
-      </div>
-    );
+    return <Transition isLoading={true} />;
   }
 
-  if (user && (user.role === "admin" || user.role === "superAdmin")) {
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (user.role === "admin" || user.role === "superAdmin") {
     return children;
   }
 
-  if (user) {
-    return <Navigate to="/profile" replace />;
-  } else {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+  return <Navigate to="/profile" replace />;
 };
 
 export default AdminRoute;
