7c5d273c5db841504568873359364280adfc19b0
Mazda Nawallsyah <mazdanawallsyah2601@gmail.com>
2025-09-09 11:57:38 +0700

    lucu nih admin routing

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -6,19 +6,15 @@ import Transition from "./LandingPage/Transition.jsx";
 const AdminRoute = ({ children }) => {
   const { user, loading } = useContext(UserContext);
   const location = useLocation();
-
   if (loading) {
     return <Transition isLoading={true} />;
   }
-
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-
   if (user.role === "admin" || user.role === "superAdmin") {
     return children;
   }
-
   return <Navigate to="/profile" replace />;
 };
 
